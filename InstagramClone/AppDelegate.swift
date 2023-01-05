02034e38ec51da47c45d3abf66263f554e03fb62
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2023-01-05 19:48:59 +0300

    initial commit

---
+++ InstagramClone/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  InstagramClone
+//
+//  Created by Oğuzhan Kaymak on 5.01.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions)
+    -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+}
---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Oğuzhan Kaymak on 5.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+}

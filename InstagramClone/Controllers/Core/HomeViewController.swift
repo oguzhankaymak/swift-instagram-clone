bdbc84a96a77db9bda60caeb3f0dc4e4bd0d89d2
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2023-01-06 00:13:37 +0300

    Create collection view in home for stories and posts

--- InstagramClone/Controllers/Core/HomeViewController.swift
+++ InstagramClone/Controllers/Core/HomeViewController.swift
@@ -8,13 +8,36 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, _) -> NSCollectionLayoutSection? in
+            switch sectionIndex {
+            case 0:
+                return CompositionalLayoutSectionHelper.createStoriesSection()
+            case 1:
+                return CompositionalLayoutSectionHelper.createPostsSection()
+            default:
+                return nil
+            }
+        }
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemBackground
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        addUIElements()
         configureNavigationBar()
+        configureCollectionView()
+        configureConstraints()
     }
 
+    private func addUIElements() {
+        view.addSubview(collectionView)
+    }
 }
 
 // MARK: - configureNavigationBar
@@ -71,5 +94,94 @@ extension HomeViewController {
 
     @objc private func console() {
         print("click")
+    }
+}
+
+// MARK: - configureCollectionView
+extension HomeViewController {
+    private func configureCollectionView() {
+        collectionView.dataSource = self
+        collectionView.register(StoryCell.self, forCellWithReuseIdentifier: StoryCell.identifier)
+        collectionView.register(PostCell.self, forCellWithReuseIdentifier: PostCell.identifier)
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension HomeViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return storiesData.count
+        case 1:
+            return postsData.count
+        default:
+            return 0
+        }
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+
+        switch indexPath.section {
+        case 0:
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: StoryCell.identifier,
+                for: indexPath
+            ) as? StoryCell else {
+                return UICollectionViewCell()
+            }
+
+            let story = storiesData[indexPath.row]
+            let storyCellModel: StoryCellViewModel = StoryCellViewModel(
+                username: story.username,
+                imagePath: story.imagePath
+            )
+            cell.configure(with: storyCellModel)
+
+            return cell
+        case 1:
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: PostCell.identifier,
+                for: indexPath
+            ) as? PostCell else {
+                return UICollectionViewCell()
+            }
+
+            let post =  postsData[indexPath.row]
+            let postCellModel: PostCellViewModel = PostCellViewModel(
+                username: post.username,
+                profileImagePath: post.profileImagePath,
+                imagePath: post.imagePath,
+                likeCount: post.likeCount,
+                commentCount: post.commentCount,
+                description: post.description,
+                dateDesc: post.dateDesc
+            )
+            cell.configure(with: postCellModel)
+
+            return cell
+        default:
+            return UICollectionViewCell()
+        }
+    }
+}
+
+// MARK: - configureConstraints
+extension HomeViewController {
+    private func configureConstraints() {
+        let collectionViewConstraints: [NSLayoutConstraint] = [
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ]
+
+        NSLayoutConstraint.activate(collectionViewConstraints)
     }
 }

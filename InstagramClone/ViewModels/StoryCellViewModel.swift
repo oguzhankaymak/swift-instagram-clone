012ad5fae8a32c22cf27a433b68c0bcf81a9e57b
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2023-01-05 23:44:01 +0300

    Create story cell view and story cell view model

---
+++ InstagramClone/ViewModels/StoryCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  StoryCellViewModel.swift
+//  InstagramClone
+//
+//  Created by Oğuzhan Kaymak on 5.01.2023.
+//
+
+import Foundation
+
+struct StoryCellViewModel {
+    let username: String
+    let imagePath: String
+}
---
+++ InstagramClone/Views/StoryCell.swift
@@ -0,0 +1,61 @@
+//
+//  TextCollectionViewCell.swift
+//  InstagramClone
+//
+//  Created by Oğuzhan Kaymak on 28.12.2022.
+//
+
+import UIKit
+import SDWebImage
+
+class StoryCell: UICollectionViewCell {
+    static let identifier = "story-cell-identifier"
+
+    private lazy var circleImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.borderWidth = 2
+        imageView.layer.cornerRadius = 56/2
+        imageView.clipsToBounds = true
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderColor = UIColor.link.cgColor
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .systemBackground
+        addUIElements()
+        configureConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("Story cell didn't implement")
+    }
+
+    private func addUIElements() {
+        contentView.addSubview(circleImageView)
+    }
+}
+
+// MARK: - configureModel
+extension StoryCell {
+    func configure(with storyCellModel: StoryCellViewModel) {
+        let imageUrl = URL(string: storyCellModel.imagePath)
+        circleImageView.sd_setImage(with: imageUrl)
+    }
+}
+
+// MARK: - configureConstraints
+extension StoryCell {
+    private func configureConstraints() {
+        let circleImageViewConstraints: [NSLayoutConstraint] = [
+            circleImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            circleImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            circleImageView.widthAnchor.constraint(equalToConstant: 56),
+            circleImageView.heightAnchor.constraint(equalToConstant: 56)
+        ]
+
+        NSLayoutConstraint.activate(circleImageViewConstraints)
+    }
+}

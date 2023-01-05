80d399d54570091cbd936dd191565459a3b51e81
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2023-01-05 20:31:41 +0300

    Create home navigation bar

--- InstagramClone/Controllers/Core/HomeViewController.swift
+++ InstagramClone/Controllers/Core/HomeViewController.swift
@@ -12,6 +12,64 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureNavigationBar()
     }
 
+}
+
+// MARK: - configureNavigationBar
+extension HomeViewController {
+    private func configureNavigationBar() {
+        navigationController?.navigationBar.tintColor = .label
+        navigationController?.navigationBar.backgroundColor = .systemBackground
+
+        let label = UILabel()
+        label.textColor = .label
+        label.text = "Instagram"
+        label.font = .systemFont(ofSize: 26, weight: .bold)
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: label)
+
+        let plusImage = UIImage(
+            systemName: "plus.square",
+            withConfiguration: UIImage.SymbolConfiguration(pointSize: 16)
+        )
+
+        let plusBarButtonItem = UIBarButtonItem(
+            image: plusImage,
+            style: .plain,
+            target: self,
+            action: #selector(console)
+        )
+
+        let heartImage = UIImage(
+            systemName: "heart",
+            withConfiguration: UIImage.SymbolConfiguration(pointSize: 16)
+        )
+
+        let heartBarButtonItem = UIBarButtonItem(
+            image: heartImage,
+            style: .plain,
+            target: self,
+            action: #selector(console)
+        )
+
+        let messageImage = UIImage(
+            systemName: "text.bubble",
+            withConfiguration: UIImage.SymbolConfiguration(pointSize: 16)
+        )
+
+        let messageBarButtonItem = UIBarButtonItem(
+            image: messageImage,
+            style: .plain,
+            target: self,
+            action: #selector(console)
+        )
+
+        navigationItem.rightBarButtonItems = [plusBarButtonItem, heartBarButtonItem, messageBarButtonItem]
+    }
+
+    @objc private func console() {
+        print("click")
+    }
 }

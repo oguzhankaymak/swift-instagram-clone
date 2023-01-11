36031133e6cff66eaa019b5f29e5aabf21fe9cd3
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2023-01-11 22:57:20 +0300

    Create search result cell view model and search result view cell

---
+++ InstagramClone/ViewModels/SearchResultCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResultCellViewModel.swift
+//  InstagramClone
+//
+//  Created by Oğuzhan Kaymak on 11.01.2023.
+//
+
+import Foundation
+
+struct SearchResultCellViewModel {
+    let username: String
+    let nameSurname: String
+    let profileImagePath: String
+}
